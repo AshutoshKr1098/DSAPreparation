eb36e15e1003031374fd96d7937415ff5a362c76
Ashutosh <ashu.heretocode@gmail.com>
2021-04-07 05:15:57 +0530

    updated question list, solved find path from root to any node

--- BinaryTree/1.TreeBuildUsingStructure.cpp
+++ BinaryTree/1.TreeBuildUsingStructure.cpp
@@ -62,7 +62,52 @@ void postorder(node *root)
     cout << root->data << " ";
     return;
 }
+bool helperPathtoNode(node *root,int n,vector<int>&v)
+{    
+    //Case-1 when we hit a NULL node 
+    if(root==NULL)
+        return false;    
+    //Case-2 When we hit a leaf node which is not equal to n
+    if(root->left==NULL && root->right==NULL && root->data!=n)
+        return false;
+    //Case-3 when we hit a leaf node which is equal to n
+    if(root->left==NULL && root->right==NULL && root->data==n)
+    {
+        v.push_back(root->data);
+        return true;
+    }
+    //Case-4 I often miss this case, this case is for intermediate nodes, which is equal to n
+    if(root->data==n)
+    {
+        v.push_back(root->data);
+        return true;
+    }
+    //Here we push again for intermediate node, but these might be popped later depending on left and right subtree's return value
+    v.push_back(root->data);
+    bool l = helperPathtoNode(root->left, n, v);
+    bool r = helperPathtoNode(root->right, n, v);
+    if(!l && !r)
+    {
+        v.pop_back();
+        return false;
+    }
+    return true;
+}
+void pathToNode(node *root,int n)
+{
+    vector<int>v;
+    bool res = helperPathtoNode(root,n,v);
+    if(res)
+    {
+        for(auto it:v)
+            cout<<it<<"->";
+    }
+    else
+        cout<<v.size()<<endl;
+
+}
 int main()
 {
     node *root = buildTreePreorder(root);
+    pathToNode(root, 5);
 }
