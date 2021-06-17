681c74dd24068fd423175f83b44e830c9c5511b1
Ashutosh <ashu.heretocode@gmail.com>
2021-06-17 18:28:54 +0530

    To solve nearest element of a number

---
+++ DividenConquer/BinarySearch/7.nearestElement.cpp
@@ -0,0 +1,5 @@
+/**
+ * Problem: Find nearest element to a number in a given array
+ * This question is different from the question find ceil or floor, as the nearest element can be any of the two
+*/
+
